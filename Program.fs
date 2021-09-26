0bae91d86f240f1de96cc5141e7d922ef9494ddf
Anton Surkis <antonsurkis@gmail.com>
2021-09-26 13:29:53 +0300

    Add map solution

--- Program.fs
+++ Program.fs
@@ -18,7 +18,7 @@ module Problem9 =
                 | Some result -> Some result
                 | None -> iterate (curr + 1) stop f
 
-        let solve n =
+        let findTriplet n =
             iterate 1 n (fun a -> iterate a (n - a) <| optionalAnswer n a)
 
     module ReduceSolution =
@@ -29,16 +29,44 @@ module Problem9 =
                         yield (a, b)
             }
 
-        let solve n =
+        let findTriplet n =
             sequence n
             |> Seq.filter (fun (a, b) -> a + b <= n)
             |> Seq.tryPick (fun (a, b) -> optionalAnswer n a b)
 
-let solve n = ()
+    module MapSolution =
+        let sequence n =
+            seq {
+                for a = 1 to n do
+                    yield! (seq { 1 .. n } |> Seq.map (fun b -> a, b))
+            }
+
+        let findTriplet n =
+            sequence n
+            |> Seq.map (fun (a, b) -> (a, b, n - a - b))
+            |> Seq.filter (fun (_, _, c) -> c >= 0)
+            |> Seq.tryFind (fun (a, b, c) -> a * a + b * b = c * c)
+
+    let solve triplet =
+        match triplet with
+        | Some (a, b, c) -> Some(a * b * c)
+        | None -> None
 
 [<EntryPoint>]
 let main _ =
     printfn "Special Pythagorean Triplet"
-    printfn "Tail recursive solution: %A" (Problem9.TailRecursiveSolution.solve 1000)
-    printfn "Reduce         solution: %A" (Problem9.ReduceSolution.solve 1000)
+    let limit = 1000
+
+    Problem9.TailRecursiveSolution.findTriplet limit
+    |> Problem9.solve
+    |> printfn "Tail recursive solution: %A"
+
+    Problem9.ReduceSolution.findTriplet limit
+    |> Problem9.solve
+    |> printfn "Reduce         solution: %A"
+
+    Problem9.MapSolution.findTriplet limit
+    |> Problem9.solve
+    |> printfn "Map-reduce     solution: %A"
+
     0
