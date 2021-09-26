f7a91a1528fe9fd16a8447357ea2d3d015a4e80b
Anton Surkis <antonsurkis@gmail.com>
2021-09-26 14:35:55 +0300

    Add recursive solutions

--- Program.fs
+++ Program.fs
@@ -25,6 +25,18 @@ module Problem9 =
         let findTriplet n =
             iterate 1 n (fun a -> iterate a (n - a) <| optionalAnswer n a)
 
+    module RecursiveSolution =
+        let rec iterate curr stop f =
+            if curr <= stop then
+                match f curr with
+                | None -> iterate (curr + 1) stop f
+                | Some result -> Some result
+            else
+                None
+
+        let findTriplet n =
+            iterate 1 n (fun a -> iterate a (n - a) <| optionalAnswer n a)
+
     module ReduceSolution =
         let findTriplet n =
             sequence n
@@ -64,6 +76,19 @@ module Problem22 =
 
         let solve names = iterate 1 (Seq.toList names)
 
+    module RecursiveSolution =
+        let rec nameScore name =
+            match Seq.tryHead name with
+            | Some c -> characterPos c + nameScore (Seq.tail name)
+            | None -> 0
+
+        let rec iterate pos (names: list<string>) =
+            match names with
+            | head :: tail -> pos * (nameScore head) + iterate (pos + 1) tail
+            | [] -> 0
+
+        let solve names = iterate 1 (Seq.toList names)
+
     module MapSolution =
         let scoreOfName pos name =
             name |> Seq.sumBy characterPos |> (*) pos
@@ -93,5 +118,6 @@ let main _ =
     printfn "\nNames scores"
     let names = Problem22.names ()
     printfn "Tail recursive solution: %A" (Problem22.TailRecursiveSolution.solve names)
-    printfn "Map solution: %A" (Problem22.MapSolution.solve names)
+    printfn "Recursive      solution: %A" (Problem22.RecursiveSolution.solve names)
+    printfn "Map            solution: %A" (Problem22.MapSolution.solve names)
     0
