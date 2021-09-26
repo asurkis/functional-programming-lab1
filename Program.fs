a030aebf1fdde05aba0649817a5d83403c236e11
Anton Surkis <antonsurkis@gmail.com>
2021-09-26 12:28:14 +0300

    Add tail recursive solution

--- Program.fs
+++ Program.fs
@@ -1,13 +1,26 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+module Problem9 =
+    let checkAB n a b =
+        let c = n - a - b
 
-open System
+        if a * a + b * b = c * c then
+            Some(a, b, c)
+        else
+            None
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+    module TailRecursiveSolution =
+        let rec iterate curr stop f =
+            if curr > stop then
+                None
+            else
+                match f curr with
+                | Some result -> Some result
+                | None -> iterate (curr + 1) stop f
+
+        let solve n =
+            iterate 1 n (fun a -> iterate a (n - a) <| checkAB n a)
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let main _ =
+    printfn "Special Pythagorean Triplet"
+    printfn "Tail recursive solution: %A" (Problem9.TailRecursiveSolution.solve 1000)
+    0
